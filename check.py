7c045d102a373f1205b69a309dc341209e991f6d
qiaohu <qiaohu2013@qq.com>
2017-03-16 19:57:19 +0800

    1

---
+++ check.py
@@ -0,0 +1,53 @@
+# coding:utf-8  #must be line 1
+# read order.xlsx to db 
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import sys
+
+reload(sys) 
+sys.setdefaultencoding('gb18030') #for UnicodeDecodeError: 'ascii' codec can't decode byte 0xa1 in position 0: ordinal not in range(128)
+
+orderType = ["新订","续订","复活"]
+
+wb = load_workbook("TTL.xlsx")
+ws_ttl = wb.get_sheet_by_name("ttl")
+ws_cmb = wb.get_sheet_by_name("cmb")
+ws_test = wb.get_sheet_by_name("test")
+
+#get  value
+payerNameList = []
+for row in tuple(ws_ttl.rows):
+  #payerName = row[4].value    
+  #payerNameU = unicode(payerName)
+  #if (payerNameU.encode('utf8') in orderType ):
+  #   payerName = row[5].value  
+  payerName = row[6].value  
+  if not payerName  :
+      continue     
+  print("get payerName:%s "% payerName)
+  payerNameList.append(payerName)
+
+rowCounter  = 0
+for row in tuple(ws_cmb.rows):
+   rowCounter = rowCounter + 1
+   paymentDesc = row[6].value  
+   paymentPrice = row[3].value    
+   print("get payment price %s, desc %s "% (paymentPrice, paymentDesc))
+   if not paymentDesc  :
+      continue     
+
+   bfind = False
+   for payerName in payerNameList:
+       if payerName in paymentDesc:
+               pos = "H"   + str(rowCounter)
+               #ws_test.cell(rowCounter, 1).value = payerName
+               ws_cmb[pos] = payerName
+               print("found writed %s "% (payerName)) 
+               bfind = True
+               break
+   if (not bfind):
+     print("failed %s %s"% (rowCounter,paymentDesc))
+ 
+  
+wb.save("TTL.xlsx")
+ 
