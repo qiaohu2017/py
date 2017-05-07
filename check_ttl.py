0b33d5cf2602f6deba487cae7dd94eb7d90ac769
qiaohu2013 <qiaohu2013@qq.com>
2017-05-07 10:40:05 +0800

    parent name deal before mater name

--- check_ttl.py
+++ check_ttl.py
@@ -1,5 +1,7 @@
+#!python2.7
 # coding:utf-8  #must be line 1
-# read order.xlsx to db 
+# ttl家长名字优先比较 170507
+# cmb 多个记录比如1270，20要调整一下
 from openpyxl import Workbook
 from openpyxl import load_workbook
 import sys
@@ -12,10 +14,11 @@ orderType = ["新订","续订","复活"]
 wb = load_workbook("TTL.xlsx")
 ws_ttl = wb.get_sheet_by_name("ttl")
 ws_cmb = wb.get_sheet_by_name("cmb")
-ws_test = wb.get_sheet_by_name("test")
+#ws_test = wb.get_sheet_by_name("test")
 
 #get  value
-payerNameDict = {}
+MaterNameDict = {}
+ParentNameDict = {}
 rowCounter  = 0
 for row in tuple(ws_ttl.rows):
   rowCounter = rowCounter + 1
@@ -26,14 +29,15 @@ for row in tuple(ws_ttl.rows):
   MaterName = row[5].value  
   if not MaterName  :
       continue     
-  print("get MaterName:%s "% MaterName)
-  payerNameDict[MaterName] = rowCounter
+  print("ttl get MaterName:%s "% MaterName)
+  MaterNameDict[MaterName] = rowCounter
 
   ParentName = row[11].value  
   if not ParentName  :
       continue     
-  print("get ParentName:%s "% ParentName)
-  payerNameDict[ParentName] = rowCounter
+  print("ttl get ParentName:%s "% ParentName)
+  ParentNameDict[ParentName] = rowCounter
+
   ws_ttl["A"+str(rowCounter)] = ""
 
 rowCounter  = 0
@@ -41,24 +45,35 @@ for row in tuple(ws_cmb.rows):
    rowCounter = rowCounter + 1
    paymentDesc = row[6].value  
    paymentPrice = row[3].value    
-   print("get payment price: %s, description: %s "% (paymentPrice, paymentDesc))
+   print("cmb get payment price: %s, description: %s "% (paymentPrice, paymentDesc))
    if not paymentDesc  :
       continue     
 
-   bfind = False
-   for payerName in payerNameDict.keys():
+   ttl_pos = ""
+   for payerName in ParentNameDict.keys():
        if payerName in paymentDesc:
-               cmb_pos = "H"   + str(rowCounter)
-               #ws_test.cell(rowCounter, 1).value = payerName
-               ws_cmb[cmb_pos] = payerName
-               print("cmb writed  %s. payerName:%s "% (cmb_pos, payerName)) 
-               ttl_pos = "A"+ str(payerNameDict[payerName])
-               ws_ttl[ttl_pos] = paymentPrice
-               print("ttl writed  %s. paymentPrice:%s "% (ttl_pos, paymentPrice)) 
-               bfind = True
-               break
-   if (not bfind):
-     print("failed %s %s"% (rowCounter,paymentDesc))
+          ttl_pos = "A"+ str(ParentNameDict[payerName])
+          break
+   if (ttl_pos == ""):
+     for payerName in MaterNameDict.keys():
+       if payerName in paymentDesc:
+          ttl_pos = "A"+ str(MaterNameDict[payerName])
+          break
+
+   if (ttl_pos == ""):   #not find
+      print("cmb record failed find payer: %s"% (paymentDesc))
+      continue           
+   
+   cmb_pos = "H"   + str(rowCounter)
+   ws_cmb[cmb_pos] = payerName      
+   print("cmb writed  %s. payerName:%s "% (cmb_pos, payerName))       
+   #ws_test.cell(rowCounter, 1).value = payerName
+   
+   ws_ttl[ttl_pos] = paymentPrice
+   print("ttl writed  %s. paymentPrice:%s "% (ttl_pos, paymentPrice)) 
+  
+ 
+     
  
   
 wb.save("TTL.xlsx")
